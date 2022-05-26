d0aab7178bc3c7bed210bd1b9116cbff674c4736
hexinyu <1213652135@qq.com>
2022-05-26 18:01:11 +0800

    修复触发器迁移报错的问题

--- pg_chameleon/lib/sql_util.py
+++ pg_chameleon/lib/sql_util.py
@@ -678,7 +678,7 @@ class DBObjectType(Enum):
 
         :return: target sql string
         """
-        return "SHOW CREATE {} %s.%s;".format(self.value.upper())
+        return "SHOW CREATE {} `%s`.`%s`;".format(self.value.upper())
 
 
 class SqlTranslator():
