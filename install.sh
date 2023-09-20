7d7a510985dc2636017c895b39bf427e2f382956
w30002722 <wangweiwei44@huawei.com>
2023-09-20 11:40:01 +0800

    兼容ctyunos系统 安装脚本 tar包安装失败 自动安装whl包

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ chameleon_path=${venv_path}/venv/bin/chameleon
 version=5.0.0
 chameleon_version="chameleon "$version
 version_str=''
+install_log=install.log
 echo $venv_path
 
 function run_build(){
@@ -10,32 +11,41 @@ function run_build(){
 }
 
 function check_chameleon(){
-  version_str=$($chameleon_path --version)
+  if [ -f $chameleon_path ];then
+      version_str=$($chameleon_path --version)
+  else
+      echo "$chameleon_path not exist.." >> $install_log
+  fi
 }
 
 function install_whl(){
   rm -rf $venv_path/venv
-  rm -rf $venv_path/python3.6.8
   python3 -m venv venv && $venv_path/venv/bin/pip3 install $venv_path/chameleon-$version-py3-none-any.whl 2>&1 > install_chameleon.log
 }
 
 
 function main(){
+  check_chameleon
+  if [ "$version_str" == "$chameleon_version" ]
+  then
+    echo "chameleon installed..." >> $install_log
+    exit 0
+  fi
   run_build
   check_chameleon
   if [ "$version_str" != "$chameleon_version" ]
   then
-    echo "Check version failed, please check build_chameleon.log,try to install whl package."
+    echo "Check version failed, please check build_chameleon.log,try to install whl package." >> $install_log
     install_whl
     check_chameleon
     if [ "$version_str" != "$chameleon_version" ]
     then
-      echo "Check version failed, please check install_chameleon.log." 
+      echo "Check version failed, please check install_chameleon.log." >> $install_log
     else
-      echo "Check version success."
+      echo "Check version success." >> $install_log
     fi
   else
-    echo "Check version success."
+    echo "Check version success." >> $install_log
   fi
 }
 
