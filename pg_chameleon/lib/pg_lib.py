c155ed5c7f0480e784bd0f99e89e7c3de9d57afd
hwhbj <xdhbj0022@163.com>
2024-09-03 09:46:30 +0800

    修复一级二级分区采用相同分区键迁移失败的问题

--- pg_chameleon/lib/pg_lib.py
+++ pg_chameleon/lib/pg_lib.py
@@ -1365,7 +1365,7 @@ class pg_engine(object):
             # add create partition
             partition_metadata = token["partition"]
             table_ddl = self.__build_create_table_mysql(table_metadata, partition_metadata, table_name, destination_schema, temporary_schema=False)
-            if table_ddl == "":
+            if table_ddl == {}:
                 return ""
             table_statement = table_ddl["table"]
             index_ddl = self.build_create_index( destination_schema, table_name, index_data)
@@ -3252,7 +3252,7 @@ class pg_engine(object):
                 # now we get the subpartition part
                 subpartition_method = self.build_sub_partition(schema, table_name, table_metadata, partition_metadata)
                 if subpartition_method == "":
-                    return ""
+                    return {}
                 table_ddl["table"] = (ddl_head + def_columns + subpartition_method + ddl_tail)
                 return table_ddl
 
@@ -3337,9 +3337,6 @@ class pg_engine(object):
         part_key = sub_partition_metadata[0]["partition_expression"].replace('`', '')
         part_key_split = part_key.split(',')
         sub_part_key = sub_partition_metadata[0]["subpartition_expression"].replace('`', '')
-        if part_key == sub_part_key:
-            print("OpenGauss databases don't support that the partition and subpartition have the same partition key for the level-two partition tables, online migration won't migrate this scenario.")
-            return ""
 
         if len(part_key_split) > self.max_range_column or \
                 (sub_partition_metadata[0]["partition_method"] != "RANGE COLUMNS" and len(part_key_split) > 1):
